b5609f25e4647d0325e64449e856415b0dbdfdbe
FERRER Simon Ext INNOV/IT-S <RKJD0737@orange.com>
2021-09-14 15:55:45 +0200

    add licence

--- js/lavallydette.js
+++ js/lavallydette.js
@@ -1,17 +1,3 @@
-/* 
-
-Copyright (C) 2021 Orange
-
-This software is confidential and proprietary information of Orange.
-You shall not disclose such Confidential Information and shall use it only
-in accordance with the terms of the agreement you entered into.
-Unauthorized copying of this file, via any medium is strictly prohibited.
-
-If you are Orange employee you shall use this software in accordance with
-the Orange Source Charter (http://infoportal-opensource.innov.intraorange/en/orange-source-charter/).
-
-*/
-
 $('#docs-navbar').navbar({sticky: true, hideSupra: true});
 $('.o-nav-local').prioritynav('Autres pages');
 

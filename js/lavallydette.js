e6d3af4764e4993f47b4e823b12a431d5473844d
Simon Ferrer <simon.ferrer.ext@orange.com>
2022-03-31 10:04:29 +0200

    Fix/issue 21 (#37)
    * field import improve
    
    * update button

--- js/lavallydette.js
+++ js/lavallydette.js
@@ -1268,9 +1268,6 @@ function localizeHTML() {
 		if (eleToLocalize !== null) {
 			eleToLocalize.innerHTML = langVallydette.template[key];
 		}
-		if (key === "selectFilesLabel") {
-			eleToLocalize.setAttribute('data-browse', langVallydette.dataBrowse);
-		}
 	});
 	
 	Object.keys(langVallydette.title).forEach(function (key) {
